2f5565a0833689c341c7250bd32935c3c60e598c
loonatiq <86408147+loonatiq@users.noreply.github.com>
2022-02-24 02:22:57 +0900

    add gallery

--- src/components/PictureList.js
+++ src/components/PictureList.js
@@ -4,7 +4,7 @@ function PictureList({ pictures }) {
   const picList = pictures.map((value) => {
     return (
       <div key={value.id} className="picture">
-        <img src={value.PictureUrl}></img>
+        <img src={value.PictureUrl} alt={value.id}></img>
       </div>
     );
   });
