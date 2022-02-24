c4e83764fa7b06ae7de07b8e74253924fa2af507
loonatiq <86408147+loonatiq@users.noreply.github.com>
2022-02-24 16:38:23 +0900

    modify from import to fetch

--- src/routes/Gallery.js
+++ src/routes/Gallery.js
@@ -1,15 +1,27 @@
 import Navbar from "../components/Navbar";
 import "./Gallery.css";
-import pics from "../gallery.json";
+// import pics from "../gallery.json";
 import PictureList from "../components/PictureList";
+import { useEffect, useState } from "react";
 
 function Gallery() {
+  const [images, setimages] = useState([]);
+  const getImages = async () => {
+    const response = await fetch("../../gallery.json");
+    const json = await response.json();
+    setimages(json);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
   return (
     <>
       <div className="bg-img-gallery">
         <Navbar />
         <div className="container">
-          <PictureList pictures={pics} />
+          <PictureList pictures={images} />
         </div>
       </div>
     </>
