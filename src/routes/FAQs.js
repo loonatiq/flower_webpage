d42937b8214221e5c39b8f0cdb288cc17a723ebf
loonatiq <86408147+loonatiq@users.noreply.github.com>
2022-04-15 00:50:16 +0900

    STYLE: delete background img fixed

--- src/routes/FAQs.js
+++ src/routes/FAQs.js
@@ -1,28 +1,11 @@
-// import { useState } from "react";
 import Navbar from "../components/Navbar";
 import "./FAQs.css";
 
 function FAQs() {
-  // const [faqToggle, setFaqToggle] = useState(false);
-
-  // const faqToggleClick = (e) => {
-  //   // setFaqToggle((faqToggle) => !faqToggle);
-  //   console.log("toggleclicked!");
-  //   console.log(e.target.children);
-  // };
-
   return (
     <>
       <div className="bg-img-faq">
         <Navbar />
-        {/* <div className="FAQcontainer">
-          <ul className="questionList">
-            <div className="question" onClick={faqToggleClick}>
-              어떻게 하나요?
-              <p className={"answerOff"}>이렇게 합니다.</p>
-            </div>
-          </ul>
-        </div> */}
       </div>
     </>
   );
